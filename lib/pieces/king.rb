6600931b307d59a57a250b249d65a40f6838a5bb
henry <hnr.leto11@gmail.com>
2020-09-10 23:21:03 -0400

    test

--- lib/pieces/king.rb
+++ lib/pieces/king.rb
@@ -14,10 +14,16 @@ class King
   def starting_moves(from, to)
     x, y = from
     moves = []
-
+    moves << [x + 1, y]
+    moves << [x - 1, y]
+    moves << [x, y + 1]
+    moves << [x, y - 1]
+    moves << [x + 1, y + 1]
+    moves << [x -1, y - 1]
+    check_moves?(to, moves)
   end
 
   def check_moves?(moves, to)
-    
+    moves.include?(to)
   end
 end
